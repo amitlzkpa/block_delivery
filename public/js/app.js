8af29fc323f4a9878e97fc6e148061def008cda0
Amit Nambiar <amit.lzkpa@gmail.com>
2018-07-11 01:01:34 -0400

    route autosuggest via backend

--- public/js/app.js
+++ public/js/app.js
@@ -15,9 +15,10 @@ $(document).ready(() => {
 	    	if(v.length < 3) return;
 	    	window.clearTimeout(srcTimer);
 	    	srcTimer = window.setTimeout(async () => {
-	    		let resp = await $.get(`http://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json?query=${v}&app_id=${APP_ID}&app_code=${APP_CODE}`);
+	    		let resp = await $.get(`/api/maps/autocomplete/?query=${v}`);
 	    		let suggestions = resp.suggestions;
 	    		let ss = suggestions.reduce((a, b) => {
+	    			// shape it for materialize dropdown
 	    			a[b.label] = null;
 	    			return a;
 	    		}, {});
@@ -40,7 +41,7 @@ $(document).ready(() => {
 	    	if(v.length < 3) return;
 	    	window.clearTimeout(dstTimer);
 	    	dstTimer = window.setTimeout(async () => {
-	    		let resp = await $.get(`http://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json?query=${v}&app_id=${APP_ID}&app_code=${APP_CODE}`);
+	    		let resp = await $.get(`/api/maps/autocomplete/?query=${v}`);
 	    		let suggestions = resp.suggestions;
 	    		let ss = suggestions.reduce((a, b) => {
 	    			a[b.label] = null;
@@ -59,8 +60,6 @@ $(document).ready(() => {
 	}
 
 
-	const APP_ID = 'JV3aky6KwOlRaB9UDq49';
-	const APP_CODE = 'V0lXUTYhyEqnV5A_t6fCxg';
 
 
 	// Routing
@@ -82,50 +81,28 @@ $(document).ready(() => {
 	});
 
 
-	var platform = new H.service.Platform({
-	  'app_id': APP_ID,
-	  'app_code': APP_CODE
-	});
 
-	var map;
+
+
+
+
+
+
+
+
+
+
+
 	var myLat = 40.730610;
 	var myLong = -73.935242;
-	var zoomLvl = 14;
 
-	var defaultLayers = platform.createDefaultLayers();
-	var $mapContainer = $('#mapContainer');
-
-	function loadMap() {
-		// $mapContainer.empty();
-		// // Instantiate (and display) a map object:
-		// map = new H.Map(
-		// 	$mapContainer[0],
-		// 	defaultLayers.normal.map,
-		// 	{
-		// 		zoom: zoomLvl,
-		// 		center: { lat: myLat, lng: myLong }
-		// 	});
-		// $('#latitude').val(myLat);
-		// $('#longitude').val(myLong);
-	}
 
 	navigator.geolocation.getCurrentPosition(
 		(p) => { 
 			[myLat, myLong] = [p.coords.latitude, p.coords.longitude];
-			loadMap();
 		},
 		(err) => {
-			loadMap();
 		});
-
-	$('#mapButton').on('click', (event) => {
-		// Block browser page load
-		event.preventDefault();
-		myLat = $('#latitude').val();
-		myLong = $('#longitude').val();
-
-		loadMap();
-	});
 
 
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config(); // read .env files
 const express = require('express');
+const { autocomplete } = require('./lib/heremaps-mirror');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -10,8 +12,20 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+
+app.get('/api/maps/autocomplete', async (req, res) => {
+  try {
+    const data = await autocomplete(req.query.query);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  } catch (error) {
+  	console.log(error);
+  }
+});
+
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
